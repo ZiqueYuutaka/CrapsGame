2c8ed1282fbd82a1ab21f0e3feae64b55c3775bd
ZiqueYuutaka <izferren@gmail.com>
2016-12-01 21:18:15 -0500

    Game complete V1

--- CrapsGameDisplay.py
+++ CrapsGameDisplay.py
@@ -8,34 +8,30 @@ MAX = 6
 counter = 0
 
 totalFunds = 0
-winnings = 0
+wins = 0
 loses = 0
 key = ''
 playerPoint=0
 name = ''
 
-def printFunds(funds):
-	print('Funds:\t$ %.2f' % (funds))
-
 def reset():
-	winnings = 0
+	wins = 0
 	loses = 0
 
-def printStats(winnings, loses):
-	print('Winnings: %d'% winnings)
+def printStats(player, funds, wins, loses):
+	print('Player:   ' + player)
+	print('Funds:  $ %.2f' % (funds))
+	print('Wins:     %d'% wins)
 	print('Loses:    %d'% loses)
 
 def getInput():
 	rollAgain = ''
 	while rollAgain != 'R' and rollAgain != 'r':
-		print(rollAgain)
 		rollAgain = input('Press \'R\' or \'r\' to roll dice: ')
 		if rollAgain == 'R' or rollAgain == 'r':
-			print('player rolled dice')
 			return rollAgain
 		else:
 			print('incorrect input')
-		print(rollAgain)
 
 def getPlayersFromFile():
 	players = {}
@@ -54,7 +50,7 @@ def getPlayersFromFile():
 	return players
 
 def getName():
-	return input('Please enter your name: ')
+	return input('Please enter your name(case sensitive): ')
 
 def getAnswer():
 	correctInput = 'false'
@@ -72,9 +68,7 @@ def getAnswer():
 #Read in data before game starts
 gameHistory = getPlayersFromFile()
 
-print(gameHistory)
 print('Welcome to CRAPS\n')
-#Ask player to enter their name
 
 playerExists = 'false'
 while playerExists == 'false':
@@ -84,12 +78,10 @@ while playerExists == 'false':
 		currentGameStats = gameHistory[name]
 		print('Welcome back ' + name + '!')
 		print('Current Game Stats:')
-		print('Funds: ' + currentGameStats[0] + '\n' +
-			  'Wins: ' + currentGameStats[1] + '\n' +
-			  'Loses: ' + currentGameStats[2] + '\n')
 		totalFunds = int(currentGameStats[0])
-		winnings = int(currentGameStats[1])
+		wins = int(currentGameStats[1])
 		loses = int(currentGameStats[2])
+		printStats(name, totalFunds, wins, loses)
 		playerExists = 'true'
 	except:
 		print('Player does not exists')
@@ -97,19 +89,18 @@ while playerExists == 'false':
 		if(newPlayer == 'Y' or newPlayer == 'y'):
 			print('Creating new player')
 			name = getName()
-			#Set current game to default settings
-			totalFunds = 0
-			winnings = 0
+			totalFunds = 1000
+			wins = 0
 			loses = 0
 			gameHistory[name] = currentGameStats
-			print(gameHistory[name])
-		playerExists = 'true'
+			playerExists = 'true'
+		#else get name
 
 while key != '5':
 	print('\n\nCRAPS\n\n\n' +
 		  '1\tPlay the game\n'+
-		  '2\tDispay Available Funds\n' +
-		  '3\tReset Winnings to Zero\n' +
+		  '2\tDisplay Available Funds\n' +
+		  '3\tReset wins to Zero\n' +
 		  '4\tSave Name and Score\n' +
 		  '5\tQUIT\n\n')
 	key = input('Key => ')
@@ -118,91 +109,90 @@ while key != '5':
 		print('Play the game\n')
 
 		if(totalFunds <= 0):
-			print('You have no available funds to play. Please reset.')
-			continue
-
-		roll = ''
-		playGame = 'true'
-		turn = 1
-		point = 0
-		while playGame == 'true':
-			roll = getInput()
-			if(roll == 'R' or roll == 'r'):
-				die1 = random.randint(MIN,MAX)
-				die2 = random.randint(MIN,MAX)
-				point = die1 + die2
-				print('die1:\t %d \n' % (die1))
-				print('die2:\t %d \n' % (die2))
-				print('Roll Total:\t %d' % (point))
-				if (point == 7 or point == 11) and turn == 1:
-					print('You rolled a 7 or 11 on turn 1')
-					print('YOU WIN!')
-					totalFunds = totalFunds + 100
-					winnings += 1
-					playGame = 'false'
-				elif (point == 2 or point == 3 or point == 12) and turn == 1:
-					print('You rolled a 2, 3 or 12 on turn 1')
-					print('YOU LOSE!')
-					totalFunds = totalFunds - 100
-					loses += 1
-					playGame = 'false'
-				else: # play using the 'point'
-					turn = turn + 1
-					#DEBUG
-					print('DEBUG Turn num: %d' % (turn))
-					print('Roll until you get a %d' % (point))
-					while playGame == 'true':
-						roll = getInput()
-						if(roll == 'R' or roll == 'r'):
-							die1 = random.randint(MIN,MAX)
-							die2 = random.randint(MIN,MAX)
-							print('You rolled a:\t %d' % (die1 + die2))
-							if(point == (die1 + die2)):
-								print('YOU WIN!')
-								playGame = 'false'
-								rollAgain = 'false'
-								totalFunds = totalFunds  + 100
-								winnings += 1
-							elif((die1 + die2) == 7):
-								print('YOU LOSE!')
-								print('You rolled a 7')
-								playGame = 'false'
-								rollAgain = 'false'
-								totalFunds = totalFunds - 100
-								loses += 1
-							else:
-								print('Roll till you get a %d!\n' % (point))
+			print('GAME OVER-YOU ARE OUT OF FUNDS')
+		else:
+			roll = ''
+			playGame = 'true'
+			turn = 1
+			point = 0
+			while playGame == 'true':
+				roll = getInput()
+				if(roll == 'R' or roll == 'r'):
+					die1 = random.randint(MIN,MAX)
+					die2 = random.randint(MIN,MAX)
+					point = die1 + die2
+					print('die1:\t %d \n' % (die1))
+					print('die2:\t %d \n' % (die2))
+					print('Roll Total:\t %d' % (point))
+					if (point == 7 or point == 11) and turn == 1:
+						print('You rolled a 7 or 11 on turn 1')
+						print('YOU WIN!')
+						totalFunds = totalFunds + 100
+						wins += 1
+						playGame = 'false'
+					elif (point == 2 or point == 3 or point == 12) and turn == 1:
+						print('You rolled a 2, 3 or 12 on turn 1')
+						print('YOU LOSE!')
+						totalFunds = totalFunds - 100
+						loses += 1
 						if(totalFunds <= 0):
-							print('You have no available funds left to play')
+							print('GAME OVER-YOU ARE OUT OF FUNDS')
 							playGame = 'false'
-							break
+					else: # play using the 'point'
+						turn = turn + 1
+						print('Roll until you get a %d' % (point))
+						while playGame == 'true':
+							roll = getInput()
+							if(roll == 'R' or roll == 'r'):
+								die1 = random.randint(MIN,MAX)
+								die2 = random.randint(MIN,MAX)
+								print('You rolled a:\t %d' % (die1 + die2))
+								if(point == (die1 + die2)):
+									print('YOU WIN!')
+									playGame = 'false'
+									rollAgain = 'false'
+									totalFunds = totalFunds  + 100
+									wins += 1
+								elif((die1 + die2) == 7):
+									print('YOU LOSE!')
+									print('You rolled a 7')
+									playGame = 'false'
+									rollAgain = 'false'
+									totalFunds = totalFunds - 100
+									loses += 1
+								else:
+									print('Roll till you get a %d!\n' % (point))
+							if(totalFunds <= 0):
+								print('GAME OVER-YOU ARE OUT OF FUNDS')
+								playGame = 'false'
+								break
 
-		printFunds(totalFunds)
-		printStats(winnings, loses)
+		printStats(name, totalFunds, wins, loses)
 
 	elif key == '2':
 		print('Display Available Funds\n')
 		print('Funds:\t$ %.2f' % (totalFunds))
-		printStats(winnings, loses)
+		printStats(name, totalFunds, wins, loses)
 
 	elif key == '3':
-		print('Reset Winnings to Zero')
+		print('Resetting game stats')
 		totalFunds = 1000
-		winnings = 0
+		wins = 0
 		loses = 0
+		printStats(name, totalFunds, wins, loses)
 
 	elif key == '4':
-		print('Save Name and Score')
 		outfile = open(r'PlayersAndScores.txt', 'w')
 		try:
-			print('Saving gameHistory to file PlayersAndScores.txt')
-			gameHistory[name] = (totalFunds, winnings, loses)
-			print(gameHistory[name])
+			print('Saving current game')
+			printStats(name, totalFunds, wins, loses)
+			gameHistory[name] = (totalFunds, wins, loses)
 			for i in gameHistory:
 				tempTup = gameHistory[i]
 				outfile.write(i + '|' + str(tempTup[2])+ '|' + 
 					str(tempTup[1])+ '|' + str(tempTup[0]) + '\n')
-
+		except:
+			print('Error saving data to file')
 		finally:
 			outfile.close()
 
@@ -211,6 +201,8 @@ while key != '5':
 		#are you sure you want to quit
 		quitGame = getAnswer()
 		if quitGame == 'Y' or quitGame == 'y':
+			print('Hope to see you soon')
+			printStats(name, totalFunds, wins, loses)
 			break
 		else:
 			key = ''
